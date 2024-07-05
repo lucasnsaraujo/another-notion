4a6b8c392f353f3cc658219cc9e1b951332e89db
Lucas Nasci <lucasnsaraujo0310@gmail.com>
2024-07-04 22:21:02 -0300

    fixed the damn sidebar button

--- src/renderer/src/layouts/default.tsx
+++ src/renderer/src/layouts/default.tsx
@@ -1,17 +1,23 @@
 import * as Collapsible from "@radix-ui/react-collapsible";
+import { useState } from "react";
+
+import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
-import { Outlet } from "react-router-dom";
 
 export function Default() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <Collapsible.Root defaultOpen={true}>
-      <div className="h-screen w-screen text-rotion-100 flex flex-row bg-rotion-900">
-        <Sidebar />
-        <div className="flex-1 flex-col flex max-h-screen">
-          <Header />
-          <Outlet />
-        </div>
+    <Collapsible.Root
+      defaultOpen
+      onOpenChange={setIsSidebarOpen}
+      className="h-screen w-screen bg-rotion-900 text-rotion-100 flex"
+    >
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header isSidebarOpen={isSidebarOpen} />
+        <Outlet />
       </div>
     </Collapsible.Root>
   );
